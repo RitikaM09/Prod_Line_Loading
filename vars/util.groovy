c121b92776c5ee85431b0681103f846f1830f5dc
sPoojarySujith <88366520+sPoojarySujith@users.noreply.github.com>
2021-09-28 15:25:10 +0530

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -18,11 +18,11 @@ void loadProperty(def parent,params)
   for (param in params) {
         String[] paramArray = param.split(":", 2)
         String key = paramArray[0]
+    println "KEY : ${key}"
         if (paramArray.length == 2) {
             String value = paramArray[1]
-            println value
+          println "VALUE : ${value}"
             parent.parameterMap.put(key.toString().trim(), value.toString().trim())
-                println parameterMap
 
         } else if (paramArray.length == 1) {
             parent.parameterMap.put(key.toString().trim(), "")
