2978069dacef8396007b36f19d933e76c7b9dbaf
sPoojarySujith <88366520+sPoojarySujith@users.noreply.github.com>
2021-09-28 13:18:47 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -1,1 +1,22 @@
-####
+#String[] params =  InputParameters.split(";")
+
+
+def initStage(def caller)
+{
+  def inputParameters="""component_name:wallets;
+environment:d1-np;
+git_repo:git@github.sie.sony.com:SIE/ca-hyperloop.git;
+project_dir:property/;
+testscope:Promote;
+git__branch:gisc-kt;"""
+  
+  String [] params= inputParameters.split(";")
+  
+  
+  util.parseInputParams(caller,params)
+
+  
+  
+  
+    
+}
