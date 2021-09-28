a51102d7c2fc327f363107dd2fe78a553e859390
sPoojarySujith <88366520+sPoojarySujith@users.noreply.github.com>
2021-09-28 17:16:02 +0530

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -50,7 +50,7 @@ void loadProperty(def parent,params)
 
 ////////////////////////////////////////////////////////////////////////////////
  void loadJobMaps(line, testType, testMap,def parent) {
-    userDataYML = readYaml text:  'property/test.yml'
+    userDataYML = readYaml file:  'property/test.yml'
     println "userDataYML : ${userDataYML}"
     //def jobData = parent.userDataYML.(indent.toString()).(testType.toString().trim())
     //jobData = (jobData == null ? "" : jobData)
