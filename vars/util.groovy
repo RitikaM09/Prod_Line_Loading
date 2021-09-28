b3be2dc37b64055b7c132538e85a841f8e2a802f
sPoojarySujith <88366520+sPoojarySujith@users.noreply.github.com>
2021-09-28 13:23:51 +0530

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -1,1 +1,30 @@
-####
+def parseInputParams(def parent,params)
+{
+  loadProperty(parent,params)
+
+}
+
+
+
+void loadProperty(def parent,params)
+{
+  for (param in params) {
+        String[] paramArray = param.split(":", 2)
+        String key = paramArray[0]
+        if (paramArray.length == 2) {
+            String value = paramArray[1]
+            println value
+            parent.parameterMap.put(key.toString().trim(), value.toString().trim())
+        } else if (paramArray.length == 1) {
+            parent.parameterMap.put(key.toString().trim(), "")
+        }
+      println "############"
+      println parameterMap
+  
+  }
+  
+  
+  
+  
+}
+
